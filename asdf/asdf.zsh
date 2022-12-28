bfb2ebab9033241b384f525bd63d848305f6a388
Joel Ambass <Jcambass@users.noreply.github.com>
2022-12-28 10:56:48 +0100

    Add ADSF

---
+++ asdf/asdf.zsh
@@ -0,0 +1,5 @@
+# Only use ASDF on Mac
+if test "$(uname)" = "Darwin"
+then
+  . $HOME/.asdf/asdf.sh
+fi
---
+++ asdf/completion.zsh
@@ -0,0 +1,5 @@
+completion='${ASDF_DIR}/completions'
+if test -f $completion
+then
+  source $completion
+fi
