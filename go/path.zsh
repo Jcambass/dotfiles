16ea13945c160801f12b9dee9005db40cbe86a8d
Joel Ambass <Jcambass@users.noreply.github.com>
2023-01-11 15:38:07 +0100

    gopath only mac

--- go/path.zsh
+++ go/path.zsh
@@ -1,2 +1,6 @@
-export GOPATH=$PROJECTS/go
-export PATH="$GOPATH/bin:$PATH"
+# Only set GOPATH on Mac
+if test "$(uname)" = "Darwin"
+then
+  export GOPATH=$PROJECTS/go
+  export PATH="$GOPATH/bin:$PATH"
+fi
