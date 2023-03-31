cc7b822a1c5aa188c5b1ee1eb43b8c3024bc405b
Joel Ambass <Jcambass@users.noreply.github.com>
2023-03-31 08:54:37 +0200

    Update set-defaults.sh

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -10,6 +10,9 @@
 # Use AirDrop over every interface. srsly this should be a default.
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
+# Enable press-and-hold for keys in favor of key repeat.
+defaults write -g ApplePressAndHoldEnabled -bool true
+
 # Always open everything in Finder's list view. This is important.
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
