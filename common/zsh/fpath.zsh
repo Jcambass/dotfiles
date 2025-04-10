96f29cbe4e2fcecb95e09ff8d79373d4149d7738
Joel Ambass <Jcambass@users.noreply.github.com>
2025-04-10 08:51:56 +0200

    fixes

--- common/zsh/fpath.zsh
+++
@@ -1,3 +0,0 @@
-#add each topic folder to fpath so that they can add functions and completion scripts
-for topic_folder ($SYSTEM_SPECIFIC_DOTFILES/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
-# TODO: Understand and replace.
