094c7a841a1177b8980973d4a99477e8284b8264
Joel Ambass <Jcambass@users.noreply.github.com>
2026-01-28 08:54:28 +0000

    fix vscode extension install

--- bpdev/vscode-extensions/install.sh
+++ bpdev/vscode-extensions/install.sh
@@ -1,6 +1,16 @@
-echo "› installing VSCode extensions"
-code --install-extension eamodio.gitlens
-code --install-extension timonwong.shellcheck
-code --install-extension nilpatel.title
-code --install-extension github.vscode-pull-request-github
-code --install-extension sleistner.vscode-fileutils
+# Detect which VS Code command is available
+if command -v code-insiders &> /dev/null; then
+  CODE_CMD="code-insiders"
+elif command -v code &> /dev/null; then
+  CODE_CMD="code"
+else
+  echo "› neither code nor code-insiders found, skipping VSCode extensions"
+  exit 0
+fi
+
+echo "› installing VSCode extensions using $CODE_CMD"
+$CODE_CMD --install-extension eamodio.gitlens
+$CODE_CMD --install-extension timonwong.shellcheck
+$CODE_CMD --install-extension nilpatel.title
+$CODE_CMD --install-extension github.vscode-pull-request-github
+$CODE_CMD --install-extension sleistner.vscode-fileutils
